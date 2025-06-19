21eafd4a585b83439bc63c55734283620d0ca120
Muhammad Faraz Ali Khan <117511582+FarazAliKhan90@users.noreply.github.com>
2025-06-19 21:16:01 +0500

    update file

--- src/portals/dr_portal/src/pages/Shedule/index.jsx
+++ src/portals/dr_portal/src/pages/Shedule/index.jsx
@@ -1,60 +1,4 @@
-// import React, { useState } from 'react';
-// import './PrescriptionForm.css';
 
-
-// const PrescriptionForm = () => {
-//     const [formData, setFormData] = useState({
-//         patientId: '',
-//         patientName: '',
-//         doctorName: '',
-//         diagnosis: '',
-//         prescription: '',
-//         additionalNotes: ''
-//     });
-
-//     const handleChange = (e) => {
-//         setFormData({ ...formData, [e.target.name]: e.target.value });
-//     };
-
-//     const handleSubmit = (e) => {
-//         e.preventDefault();
-//         console.log(formData);
-//         alert("Prescription Submitted Successfully!");
-//     };
-
-//     return (
-//         <section className="prescription-section">
-//             <form className="prescription-form" onSubmit={handleSubmit}>
-//                 <h2 className="form-title">Prescription & Managment</h2>
-//                 <div className="input-wrapper">
-//                     <label>Patient ID</label>
-//                     <input type="text" name="patientId" className="input-field" value={formData.patientId} onChange={handleChange} required />
-//                 </div>
-//                 <div className="input-group">
-//                     <div className="input-wrapper">
-//                         <label>Patient Name</label>
-//                         <input type="text" name="patientName" className="input-field" value={formData.patientName} onChange={handleChange} required />
-//                     </div>
-//                     <div className="input-wrapper">
-//                         <label>Doctor Name</label>
-//                         <input type="text" name="doctorName" className="input-field" value={formData.doctorName} onChange={handleChange} required />
-//                     </div>
-//                 </div>
-//                 <div className="input-wrapper">
-//                     <label>Prescription</label>
-//                     <textarea name="prescription" className="review-box" value={formData.prescription} onChange={handleChange} required></textarea>
-//                 </div>
-//                 <div className="input-wrapper">
-//                     <label>Additional Notes</label>
-//                     <textarea name="additionalNotes" className="review-box" value={formData.additionalNotes} onChange={handleChange}></textarea>
-//                 </div>
-//                 <button type="submit" className="submit-button">Submit</button>
-//             </form>
-//         </section>
-//     );
-// };
-
-// export default PrescriptionForm;
 
 
 import React, { useState } from 'react';
@@ -99,7 +43,7 @@ const PrescriptionForm = () => {
             <form className="prescription-form" onSubmit={handleSubmit}>
                 <h2 className="form-title">Prescription & Management</h2>
                 <div className="input-wrapper">
-                    <label>Patient ID</label>
+                    <label>Patient ho bahi  ID</label>
                     <input type="text" name="patientId" className="input-field" value={formData.patientId} onChange={handleChange} required />
                 </div>
                 <div className="input-group">
